bf8cf69c67a3e8b10859c06669f0d1dedb55168e
Chi Nguyen <buichinguyen1105@gmail.com>
2024-09-19 13:49:48 +0300

    @@!

--- week38/e02/App.java
+++ week38/e02/App.java
@@ -2,6 +2,21 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int score = scanner.nextInt();
+        scanner.close();
 
+        if (score >= 90) {
+            System.out.println("Excellent!");
+        }
+        else if (score >= 70) {
+            System.out.println("Good Job!");
+        }
+        else if (score >= 50) {
+            System.out.println("Needs Improvement.");
+        }
+        else {
+            System.out.println("Fail.");
+        }
     }
 }
