902968652c10a641cbcc3db559ff22d6dc00de91
Chi Nguyen <buichinguyen1105@gmail.com>
2024-09-19 14:07:19 +0300

    ...

--- week38/e02/App.java
+++ week38/e02/App.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Input your score");
         int score = scanner.nextInt();
         scanner.close();
 
---
+++ week38/e03/App.java
@@ -0,0 +1,19 @@
+package e03;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Your age is:");
+        int age = scanner.nextInt();
+        System.out.println("Is this the weekend? (true or false):");
+        boolean isWeekend = scanner.nextBoolean();
+        if (!isWeekend && (age < 18 || age > 65)) {
+            System.out.println("You will have a discount!");
+        }
+        else {
+            System.out.println("You have to pay full price.");
+        }
+        scanner.close();
+    }
+}
