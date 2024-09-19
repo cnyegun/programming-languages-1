efacdfa3bef1615caf214cd24abbb360977fb134
Chi Nguyen <buichinguyen1105@gmail.com>
2024-09-19 13:38:45 +0300

    homework

---
+++ week38/e01/App.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your age: ");
+        int age = scanner.nextInt();
+        scanner.close();
+        if (age >= 18) {
+           System.out.println("You are eligible to vote."); 
+        }
+        else{
+            System.out.println("You are not eligible to vote.");
+        }
+    }
+}
