443762a8e970da9222f75e8d6f565239c4cd9e16
Chi Nguyen <buichinguyen1105@gmail.com>
2024-09-28 17:12:09 +0300

    changes

--- week_39/e03/App.java
+++ week_39/e03/App.java
@@ -1,6 +1,28 @@
 package e03;
 public class App{
+	public static String multiInt(int times, int c) {
+		int n = times;
+		String rStr = "";
+		while (n > 0) {
+			rStr = rStr + c;
+			n -= 1;
+		}
+		return rStr;
+	}
+	public static String multiChar(int times, char c) {
+		int n = times;
+		String rStr = "";
+		while (n > 0) {
+			rStr = rStr + c;
+			n -= 1;
+		}
+		return rStr;
+	}
     public static void main(String[] args) {
-
+        int loops = 6;
+        for (int i = 1 ; i <= loops; i++) {
+            System.out.print(multiChar(loops - i,' '));
+            System.out.print(multiInt(i, i) + '\n');
+        }
     }
 }
