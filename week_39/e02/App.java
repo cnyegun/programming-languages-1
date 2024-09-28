1d378ca51add0fb75c58009e5ef63d95cecc1f51
Chi Nguyen <buichinguyen1105@gmail.com>
2024-09-28 16:57:35 +0300

    changes

--- week_39/e02/App.java
+++ week_39/e02/App.java
@@ -1,15 +1,26 @@
 public class App {
-	public static String printBlank(int blank_space) {
-		int n = blank_space;
+	public static String multiChar(int times, char c) {
+		int n = times;
 		String rStr = "";
 		while (n > 0) {
-			rStr = rStr + "_";
+			rStr = rStr + c;
 			n -= 1;
 		}
 		return rStr;
 	}
    	public static void main(final String[] args) {
-		String tempStr = 
+		int star = 1;
+		int star_increment = 2;
+		int space = 3;
+		int space_decrement = -1;
+
+		for (int i = 0; i < 4 ; i++) {
+			System.err.print(multiChar(space, ' '));
+			System.err.print(multiChar(star, '*'));
+			System.out.println();
+			star += star_increment;
+			space += space_decrement;
+		}
 	}
 }
 
