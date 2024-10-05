4f7a7b88e9b596cbb931feaaf315b872410f0ea9
Chi Nguyen <buichinguyen1105@gmail.com>
2024-10-05 15:30:47 +0300

    changes

--- week40/e06/App.java
+++ week40/e06/App.java
@@ -1,5 +1,21 @@
-public class Main {
+import java.util.*;
+public class App {
     public static void main(String[] args) {
+        System.out.println("Palindrome test, please enter some text: ");
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        if(s.equalsIgnoreCase(reverseString(s))) {
+            System.out.println(s + " is a palindrome");
+        }
+        else {
+            System.out.println(s + " is not a palindrome");
+        }
+    }
+    public static String reverseString(String s) {
+        String rString = "";
+        for(int i = s.length(); i > 0; i--) {
+            rString = rString.concat(s.substring(i - 1, i));
+        }
+        return rString;
     }
 }
-
