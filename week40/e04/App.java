e1c267107439a5a871c8aacb00e1f4542a5ef678
Chi Nguyen <buichinguyen1105@gmail.com>
2024-10-05 15:05:19 +0300

    changes

--- week40/e04/App.java
+++ week40/e04/App.java
@@ -1,5 +1,15 @@
-public class Main {
+import java.util.*;
+public class App {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a name: ");
+        String name = scanner.nextLine();
+        System.out.println("You entered: " + name);
+        for (int i = 0; i < name.length(); i++) {
+            if ((i + 1) % 2 == 0) {
+                System.out.println(name.charAt(i));
+            }
+        }
+        scanner.close();
     }
 }
-
