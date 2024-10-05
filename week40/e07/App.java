a266e8a9f6b80d9172cc51ba24e721100712b76f
Chi Nguyen <buichinguyen1105@gmail.com>
2024-10-05 16:01:50 +0300

    changes

--- week40/e07/App.java
+++ week40/e07/App.java
@@ -1,5 +1,26 @@
-public class Main {
+import java.util.*;
+public class App {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String passString = "";
+        Boolean valid = false, upperCase = false, lowerCase = false, hasDigit = false;
+
+        while(!valid) {
+            System.out.println("Input: ");
+            passString = scanner.nextLine();
+            for(int i = 0; i < passString.length(); i++) {
+                if(passString.charAt(i) >= 48 && passString.charAt(i) <= 57) {
+                    hasDigit = true;
+                }
+                if(passString.charAt(i) >= 97 && passString.charAt(i) <= 122) {
+                    lowerCase = true;
+                }
+                if(passString.charAt(i) >= 65 && passString.charAt(i) <= 90) {
+                    upperCase = true;
+                }
+            }
+            valid = hasDigit && lowerCase && upperCase;
+        } 
+        scanner.close();
     }
 }
-
